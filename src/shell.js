a5a7aff5cac04c23aaf463a9f0582728875b8daa
_MoonLight_ <moonlight8978@gmail.com>
2019-07-19 21:24:32 +0700

    fix shell

--- src/shell.js
+++ src/shell.js
@@ -1,13 +1,15 @@
 const shell = require('shelljs')
 
+const exec = (command, options = {}) => shell.exec(command, { shell: '/usr/bin/zsh', ...options })
+
 module.exports = {
   ...shell,
-  exec: (command, options = {}) => shell.exec(command, { shell: '/usr/bin/zsh', ...options }),
-  check: command => this.exec(`command -v ${command}`).code === 127,
-  install: (...packages) => this.exec(`sudo apt install -y ${packages.join(' ')}`),
-  update: () => this.exec('sudo apt update -qq'),
-  upgrade: () => this.exec('sudo apt upgrade -y'),
-  addRepository: repo => this.exec(`sudo add-apt-repository -y ${repo}`),
-  removeRepository: repo => this.exec(`sudo add-apt-repository -y -r ${repo}`),
-  source: (file = '~/.zshrc') => this.exec(`source ${file}`),
+  exec,
+  check: command => exec(`command -v ${command}`).code === 127,
+  install: (...packages) => exec(`sudo apt install -y ${packages.join(' ')}`),
+  update: () => exec('sudo apt update -qq'),
+  upgrade: () => exec('sudo apt upgrade -y'),
+  addRepository: repo => exec(`sudo add-apt-repository -y ${repo}`),
+  removeRepository: repo => exec(`sudo add-apt-repository -y -r ${repo}`),
+  source: (file = '~/.zshrc') => exec(`source ${file}`),
 }
