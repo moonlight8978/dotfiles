7e1fb9c6807500a586f7ec29186fd073a47c4b9f
moonlight8978 <moonlight8978@gmail.com>
2024-04-09 17:34:31 +0700

    feat: discord components

--- .github/actions/discord-message/src/main.ts
+++ .github/actions/discord-message/src/main.ts
@@ -1,62 +1,75 @@
 import * as core from '@actions/core'
-import axios, { AxiosResponse } from 'axios'
 import { string, object } from 'yup'
 import { parseColumns } from '@munkit/column'
+import {
+  WebhookClient,
+  EmbedBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle
+} from 'discord.js'
 
 export async function run(): Promise<void> {
   const schema = object({
     message: string().required().trim(),
     columns: string().required().trim(),
     webhookUrl: string().required().trim(),
-    color: string().required().trim()
+    color: string().required().trim(),
+    threadId: string().trim().optional().nullable()
   })
 
   const inputs = schema.cast({
     message: core.getInput('message'),
     columns: core.getInput('columns'),
     webhookUrl: core.getInput('webhook-url'),
-    color: core.getInput('color')
+    color: core.getInput('color'),
+    threadId: core.getInput('thread-id')
   })
 
-  const embeds = parseColumns(inputs.columns).map(column => ({
-    name: column.title,
-    value: column.content,
-    inline: column.variant === 'inline'
-  }))
+  const client = new WebhookClient({
+    url: inputs.webhookUrl
+  })
 
-  await axios
-    .post(
-      inputs.webhookUrl,
-      {
-        content: inputs.message,
-        embeds:
-          embeds.length > 0
-            ? [
-                {
-                  color: inputs.color,
-                  fields: embeds
-                }
-              ]
-            : undefined
-      },
-      {
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        params: {
-          wait: 'true'
-        }
-      }
+  const columns = parseColumns(inputs.columns)
+  const embed = new EmbedBuilder().setColor(inputs.color as any).addFields(
+    columns
+      .filter(column => ['inline', 'full'].includes(column.variant))
+      .map(column => ({
+        name: column.title,
+        value: column.content,
+        inline: column.variant === 'inline'
+      }))
+  )
+
+  const components = columns
+    .filter(column =>
+      ['button'].some(variant => column.variant.includes(variant))
     )
-    .then(() => {
-      core.info('Message sent')
-    })
-    .catch(err => {
-      core.error(err.message)
-      if (err.response) {
-        core.error(err.response.data)
-        core.error(err.response.request.body)
+    .map(column => {
+      if (column.variant.includes('button')) {
+        const style = ((column.variant.split(':')[1] as any) ??
+          'PRIMARY') as ButtonStyle
+        return new ActionRowBuilder<ButtonBuilder>().addComponents(
+          new ButtonBuilder()
+            .setStyle(style)
+            .setLabel(column.title)
+            .setURL(column.content)
+        )
       }
-      core.setFailed(err.message)
+
+      throw new Error('Invalid column variant')
     })
+
+  try {
+    await client.send({
+      embeds: [embed],
+      content: inputs.message,
+      threadId: inputs.threadId || undefined,
+      components: components
+    })
+    core.info('Message sent')
+  } catch (err: any) {
+    core.error(err.message)
+    core.setFailed(err.message)
+  }
 }
