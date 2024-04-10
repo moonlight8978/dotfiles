b7b35f362e2f5a57119b72d15c8e055bda3ea2d0
Lê Sĩ Bích <moonlight8978@gmail.com>
2024-04-10 11:24:27 +0700

    feat: support bot client with components (#20)

--- .github/actions/discord-message/src/main.ts
+++ .github/actions/discord-message/src/main.ts
@@ -1,62 +1,184 @@
 import * as core from '@actions/core'
-import axios, { AxiosResponse } from 'axios'
-import { string, object } from 'yup'
-import { parseColumns } from '@munkit/column'
+import { string, object, number } from 'yup'
+import { Column, parseColumns } from '@munkit/column'
+import {
+  WebhookClient,
+  EmbedBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  Client,
+  GatewayIntentBits,
+  Events,
+  TextChannel,
+  ThreadChannel
+} from 'discord.js'
+
+class DiscordBotClient {
+  #token: string
+  #channelId: string
+  #client!: Client
+
+  constructor(inputs: any) {
+    const { token, channel } = object({
+      token: string().required().trim(),
+      channel: string().required().trim()
+    }).cast(inputs)
+
+    this.#token = token
+    this.#channelId = channel
+  }
+
+  async bootstrap() {
+    core.info('Bootstrapping bot client')
+    this.#client = await new Promise((resolve, reject) => {
+      const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+
+      client.once(Events.ClientReady, async readyClient => {
+        core.info(`Bot is ready. ${readyClient.user.tag}`)
+        resolve(readyClient)
+        client.destroy()
+      })
+
+      client.once(Events.Error, error => {
+        core.error(error.message)
+        core.setFailed(error.message)
+
+        reject(error)
+      })
+
+      client.login(this.#token)
+    })
+  }
+
+  async sendMessage(color: number, message: string, columns: Column[]) {
+    const channel = (await this.#client.channels.fetch(this.#channelId)) as
+      | TextChannel
+      | ThreadChannel
+      | null
+
+    if (!channel) {
+      throw new Error(`Channel not found: ${this.#channelId}`)
+    }
+
+    const embedColumns = columns.filter(column =>
+      ['inline', 'full'].includes(column.variant)
+    )
+
+    const embed = new EmbedBuilder().setColor(color).addFields(
+      embedColumns.map(column => ({
+        name: column.title,
+        value: column.content,
+        inline: column.variant === 'inline'
+      }))
+    )
+
+    const components = columns
+      .filter(column =>
+        ['button'].some(variant => column.variant.includes(variant))
+      )
+      .map(column => {
+        if (column.variant.includes('button')) {
+          const style = ((column.variant.split(':')[1] as any) ??
+            ButtonStyle.Primary) as ButtonStyle
+          return new ButtonBuilder()
+            .setStyle(style)
+            .setLabel(column.title)
+            .setURL(column.content)
+        }
+
+        throw new Error('Invalid column variant')
+      })
+
+    core.info(`embeds: ${embedColumns.length}`)
+    core.info(`components: ${components.length}`)
+
+    await channel.send({
+      embeds: embedColumns.length ? [embed] : undefined,
+      content: message || undefined,
+      components: components.length
+        ? [new ActionRowBuilder<ButtonBuilder>().addComponents(components)]
+        : undefined
+    })
+  }
+}
+
+class DiscordWebhookClient {
+  #url: string
+  #threadId?: string
+  #client!: WebhookClient
+
+  constructor(inputs: any) {
+    const { webhookUrl, threadId } = object({
+      webhookUrl: string().required().trim(),
+      threadId: string().optional().nullable()
+    }).cast(inputs)
+
+    this.#url = webhookUrl
+    this.#threadId = threadId || undefined
+  }
+
+  async bootstrap() {
+    core.info('Bootstrapping webhook client')
+    this.#client = new WebhookClient({ url: this.#url })
+  }
+
+  async sendMessage(color: number, message: string, columns: Column[]) {
+    const embedColumns = columns.filter(column =>
+      ['inline', 'full'].includes(column.variant)
+    )
+
+    const embed = new EmbedBuilder().setColor(color).addFields(
+      embedColumns.map(column => ({
+        name: column.title,
+        value: column.content,
+        inline: column.variant === 'inline'
+      }))
+    )
+
+    await this.#client.send({
+      embeds: embedColumns.length ? [embed] : undefined,
+      content: message || undefined,
+      threadId: this.#threadId
+    })
+  }
+}
 
 export async function run(): Promise<void> {
-  const schema = object({
-    message: string().required().trim(),
-    columns: string().required().trim(),
-    webhookUrl: string().required().trim(),
-    color: string().required().trim()
-  })
+  const isBotClient = !!core.getInput('token')
 
-  const inputs = schema.cast({
+  const client = isBotClient
+    ? new DiscordBotClient({
+        token: core.getInput('token'),
+        channel: core.getInput('channel')
+      })
+    : new DiscordWebhookClient({
+        webhookUrl: core.getInput('webhook-url'),
+        threadId: core.getInput('thread-id')
+      })
+
+  const messageInputs = object({
+    message: string().trim(),
+    columns: string().required().trim(),
+    color: number().integer().required()
+  }).cast({
     message: core.getInput('message'),
     columns: core.getInput('columns'),
-    webhookUrl: core.getInput('webhook-url'),
     color: core.getInput('color')
   })
 
-  const embeds = parseColumns(inputs.columns).map(column => ({
-    name: column.title,
-    value: column.content,
-    inline: column.variant === 'inline'
-  }))
+  const columns = parseColumns(messageInputs.columns)
 
-  await axios
-    .post(
-      inputs.webhookUrl,
-      {
-        content: inputs.message,
-        embeds:
-          embeds.length > 0
-            ? [
-                {
-                  color: inputs.color,
-                  fields: embeds
-                }
-              ]
-            : undefined
-      },
-      {
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        params: {
-          wait: 'true'
-        }
-      }
+  try {
+    await client.bootstrap()
+    await client.sendMessage(
+      messageInputs.color,
+      messageInputs.message || '',
+      columns
     )
-    .then(() => {
-      core.info('Message sent')
-    })
-    .catch(err => {
-      core.error(err.message)
-      if (err.response) {
-        core.error(err.response.data)
-        core.error(err.response.request.body)
-      }
-      core.setFailed(err.message)
-    })
+    core.info('Message sent')
+  } catch (err: any) {
+    core.error(err.message)
+    core.setFailed(err.message)
+  }
 }
