1e1206bf499a83890843e4b2c0b0d8f4e4e323bc
moonlight8978 <moonlight8978@gmail.com>
2024-12-19 16:47:47 +0700

    feat: move tree-sitter to lsp, add nvim comment

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -23,4 +23,31 @@ return {
 		end,
 	},
 	{ "sbdchd/neoformat" },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"lua",
+					"vim",
+					"javascript",
+					"typescript",
+					"tsx",
+					"markdown",
+					"markdown_inline",
+					"ruby",
+					"python",
+					"dockerfile",
+					"yaml",
+					"json",
+				},
+				highlight = { enable = true },
+				indent = { enable = true },
+				matchup = {
+					enable = true,
+				},
+			})
+		end,
+	},
 }
