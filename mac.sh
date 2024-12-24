a17ab12c559c5583cc7c3cd259bcf4be848d39c1
moonlight8978 <moonlight8978@gmail.com>
2024-12-24 14:01:54 +0700

    chore: install docker client

--- mac.sh
+++ mac.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-brew install stow just fzf luarocks
+brew install stow just fzf luarocks docker
 
 brew install --cask font-hurmit-nerd-font
 brew install --cask font-meslo-lg-nerd-font
