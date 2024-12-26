9db88b1c87191399dbe7727b246f5d839d5ba0dd
moonlight8978 <moonlight8978@gmail.com>
2024-12-26 22:23:56 +0700

    fix: change terminal esc to M-esc for lazygit

--- nvim/lua/config/toggleterm.lua
+++ nvim/lua/config/toggleterm.lua
@@ -6,7 +6,7 @@ local Terminal = require("toggleterm.terminal").Terminal
 
 function _G.set_terminal_keymaps()
 	local opts = { buffer = 0 }
-	vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+	vim.keymap.set("t", "<M-esc>", [[<C-\><C-n>]], opts)
 	vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
 	vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
 	vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
@@ -46,7 +46,7 @@ set("n", "<leader>t", function()
 end)
 
 -- lazygit
-local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
+local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float", start_in_insert = true })
 
 vim.keymap.set("n", "<leader>g", function()
 	lazygit:toggle()
