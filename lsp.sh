723b39bc407f79fae45284f3b90ab1743a6dce17
_MoonLight_ <work@bichls.dev>
2025-01-01 01:56:45 +0700

    feat: ansible playbook for setting up mac machine

--- lsp.sh
+++
@@ -1,8 +0,0 @@
-brew install lua-language-server stylua
-brew install hashicorp/tap/terraform-ls
-
-npm i -g \
-  dockerfile-language-server-nodejs \ 
-  bash-language-server \ 
-  @vtsls/language-server \
-  graphql-language-service-cli
--- zsh/.zshenv
+++ zsh/.zshenv
@@ -1,1 +1,2 @@
 . "$HOME/.cargo/env"
+. "$HOME/.asdf/asdf.sh"
