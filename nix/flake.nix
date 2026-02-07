fb4ce59a88bd957b71f2039f0f81164b1b84df28
_MoonLight_ <moonlight8978@gmail.com>
2026-02-07 19:50:11 +0700

    chore: update flake

--- nix/flake.nix
+++ nix/flake.nix
@@ -42,82 +42,25 @@
       ];
       specialArgs = inputs;
     };
+
+    darwinConfigurations."IBM-5100-mini" = nix-darwin.lib.darwinSystem rec {
+      system = "aarch64-darwin";
+      pkgs = mkNixpkgs system;
+      modules = homebrew.modules ++ [
+        home-manager.darwinModules.home-manager
+        ./hosts/darwin
+      ];
+      specialArgs = inputs;
+    };
+
+    darwinConfigurations."May-tinh-Casio-13-M1" = nix-darwin.lib.darwinSystem rec {
+      system = "aarch64-darwin";
+      pkgs = mkNixpkgs system;
+      modules = homebrew.modules ++ [
+        home-manager.darwinModules.home-manager
+        ./hosts/darwin
+      ];
+      specialArgs = inputs;
+    };
   };
-
-  # outputs = inputs @ {
-  #   self,
-  #   nix-darwin,
-  #   nixpkgs,
-  #   home-manager,
-  #   nix-homebrew,
-  #   nix-flatpak,
-  #   ...
-  # }: let
-  #   linuxSystems = ["x86_64-linux"];
-  #   darwinSystems = ["aarch64-darwin"];
-  #   forAllSystems = f: nixpkgs.lib.genAttrs (linuxSystems ++ darwinSystems) f;
-  #   devShell = system: let
-  #     pkgs = nixpkgs.legacyPackages.${system};
-  #   in {
-  #     default = with pkgs;
-  #       mkShell {
-  #         nativeBuildInputs = with pkgs; [git];
-
-  #         shellHook = with pkgs; ''
-  #           export EDITOR=nvim
-  #         '';
-  #       };
-  #   };
-  # in {
-  #   devShells = forAllSystems devShell;
-
-  #   homeConfigurations."moonlight" = home-manager.lib.homeManagerConfiguration rec {
-  #     pkgs = import nixpkgs {
-  #       system = "x86_64-linux";
-  #       config = {
-  #         allowUnfree = true;
-  #         allowUnfreePredicate = _: true;
-  #       };
-  #     };
-  #     modules = [
-  #       nix-flatpak.homeManagerModules.nix-flatpak
-  #       ./hosts/debian
-  #     ];
-  #     extraSpecialArgs = inputs;
-  #   };
-
-  #   darwinConfigurations."IBM-5100-mini" = nix-darwin.lib.darwinSystem rec {
-  #     system = "aarch64-darwin";
-  #     pkgs = import nixpkgs {
-  #       inherit system;
-  #       config = {
-  #         allowUnfree = true;
-  #         allowUnfreePredicate = _: true;
-  #       };
-  #     };
-  #     modules = [
-  #       home-manager.darwinModules.home-manager
-  #       nix-homebrew.darwinModules.nix-homebrew
-  #       ./hosts/darwin
-  #     ];
-  #     specialArgs = inputs;
-  #   };
-
-  #   darwinConfigurations."May-tinh-Casio-13-M1" = nix-darwin.lib.darwinSystem rec {
-  #     system = "aarch64-darwin";
-  #     pkgs = import nixpkgs {
-  #       inherit system;
-  #       config = {
-  #         allowUnfree = true;
-  #         allowUnfreePredicate = _: true;
-  #       };
-  #     };
-  #     modules = [
-  #       home-manager.darwinModules.home-manager
-  #       nix-homebrew.darwinModules.nix-homebrew
-  #       ./hosts/darwin
-  #     ];
-  #     specialArgs = inputs;
-  #   };
-  # };
 }
